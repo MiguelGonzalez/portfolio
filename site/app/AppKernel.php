3da16e09df415a3abd9fff8c17a2dcbb37bb8527
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com>
2017-06-30 00:23:48 +0200

    Fork Assetic Angular Js Bundle dependency for imprevement

--- site/app/AppKernel.php
+++ site/app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Asoc\AsseticAngularJsBundle\AsocAsseticAngularJsBundle(),
+            new Miguel\AsseticAngularJsBundle\MiguelAsseticAngularJsBundle(),
             new ContactBundle\ContactBundle(),
             new SkillsBundle\SkillsBundle(),
             new EducationBundle\EducationBundle(),
